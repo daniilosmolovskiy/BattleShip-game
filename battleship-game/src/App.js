1ff45850d82a0cd62e375ac5ad708637d908db19
Daniil <danielosmolovskiy@gmail.com>
2020-04-07 13:31:35 +0300

    useState added

--- battleship-game/src/App.js
+++ battleship-game/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from 'styled-components'
 
 const fightField = new Array(8).fill(new Array(8).fill(0));
@@ -19,14 +19,18 @@ const Cell = styled.div`
   border: 1px solid black;
 `;
 
+function App() {
+
+  const [shipPosition, setShipPosition] = useState([])
+
 const getCell = () => {
   const rowShip = parseInt((Math.random() * 8).toFixed(0))
   const cellShip = parseInt((Math.random() * 8).toFixed(0))
+  
+  setShipPosition([rowShip, cellShip])
 
   console.log(rowShip, cellShip)
 }
-
-function App() {
 
   useEffect(() => {
     getCell()
@@ -40,7 +44,11 @@ function App() {
           <Row key={index}>
             Row: {index + 1}
             {row.map((field, i) => {
-              return <Cell className={[index + 1, i + 1]} key={i}>Field: {i}</Cell>;
+              if( index === shipPosition[0] || i === shipPosition[1]) {
+                console.log('test')
+              } else {
+                return <Cell className={[index + 1, i + 1]} key={i}>Field: {i}</Cell>;
+              }
             })}
           </Row>
         );
